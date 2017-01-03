61cc6e2e731a9f1eccf4e19abc82dd0314eec4d0
guigui2121 <jauffret@intechinfo.fr>
2017-01-03 21:32:08 +0100

    accueil.php

---
+++ accueil.php
@@ -0,0 +1,57 @@
+<?php
+if(!isset($_GET["entreprise"])){
+        echo"Bienvenue sur Overview ! <br/> Voici les entreprises disponibles : ";
+        $entreprises = [];
+        exec("ls /var/projects",$entreprises);
+        foreach ($entreprises as $nom) {
+                echo "<br/>";
+                echo "<a href='./index.php?entreprise=".$nom."'>".$nom."</a>";
+        }
+}elseif(!isset($_GET["groupe"])){
+            $groupes = [];
+        exec("ls /var/www/overview/projets/".$_GET["entreprise"]."/inventaire",$groupes);
+        foreach ($groupes as $nom) {
+                echo "<br/>";
+                echo "<a href='./index.php?entreprise=".$_GET["entreprise"]."&groupe=".$nom."'>".$nom."</a>";
+        }
+}elseif(!isset($_GET["machine"])){
+            $machines = [];
+        exec("ls /var/www/overview/projets/".$_GET["entreprise"]."/inventaire/".$_GET["groupe"],$machines);
+        foreach ($machines as $nom) {
+                echo "<br/>";
+                echo "<a href='./index.php?entreprise=".$_GET["entreprise"]."&groupe=".$_GET["groupe"]."&machine=".$nom."'>".$nom."</a>";
+        }
+}elseif(!isset($_GET["c"])){
+            $categories = ["supervision","inventaire","alertes"];
+        #exec("ls /var/www/overview/projets/".$_GET["entreprise"]."/inventaire/".$_GET["groupe"],$machines);
+        foreach ($categories as $nom) {
+                echo "<br/>";
+                echo "<a href='./index.php?entreprise=".$_GET["entreprise"]."&groupe=".$_GET["groupe"]."&machine=".$_GET["machine"]."&c=".$nom."'>".$nom."</a>";
+        }
+}elseif(isset($_GET["c"])){
+            switch ($_GET["c"]){
+                case "supervision":
+                    $graphes=[];
+                    exec("ls /var/www/overview/projets/".$_GET["entreprise"]."/supervision/".$_GET["machine"]."/graphes",$graphes);
+                    foreach ($graphes as $nom) {
+                        echo "<br/>";
+                        echo "<img src='".$nom."'>";
+                    }
+                    break;
+                case "inventaire":
+                    exec("cat /var/www/overview/projets/".$_GET["entreprise"]."/inventaire/".$_GET["groupe"]."/".$_GET["machine"]."/ram_taille",$valeur);
+                    echo $valeur;
+                    
+
+                    break;
+                case "alertes":
+                    $lignes=[];
+                    exec("cat /var/www/overview/projets/".$_GET["entreprise"]."/supervision/".$_GET["machine"]."alertes",$fichiers);
+                    foreach ($lignes as $nom) {
+                        echo "<br/>";
+                        echo $nom;
+                    }
+                    break;
+            }
+}
+?>
