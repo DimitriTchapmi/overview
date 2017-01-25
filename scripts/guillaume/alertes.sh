166ded90563e6d24fdb9d0bee813b9405af97816
guigui2121 <jauffret@intechinfo.fr>
2017-01-25 20:04:53 +0000

    script alertes

--- scripts/guillaume/alertes.sh
+++ scripts/guillaume/alertes.sh
@@ -5,35 +5,40 @@
 entreprise=`echo $1 | cut -d _ -f1`
 machine=`echo $1 | cut -d _ -f2`
 
-ligne=`cat /var/www/overview/projets/$entreprise/supervision/$machine`
+ligne=`cat /var/www/overview/projets/$entreprise/supervision/$machine/alerte | grep $2$3`
+num_ligne=`cat /var/www/overview/projets/$entreprise/supervision/$machine/alerte | grep $2$3 | cut -d : -f 1`
 
-$nom_item=`echo $ligne | cut -d : -f 1`
-$seuil=`echo $ligne | cut -d : -f 2`
-$battement=`echo $ligne | cut -d : -f 3`
-$temps_atteint=`echo $ligne | cut -d : -f 4`
-$temps_redescendu=`echo $ligne | cut -d : -f 5`
-$flag=`echo $ligne | cut -d : -f 6`
+nom_item=`echo $ligne | cut -d : -f 1`
+seuil=`echo $ligne | cut -d : -f 2`
+battement=`echo $ligne | cut -d : -f 3`
+temps_atteint=`echo $ligne | cut -d : -f 4`
+temps_redescendu=`echo $ligne | cut -d : -f 5`
+flag=`echo $ligne | cut -d : -f 6`
 
-if [$flag -eq 1 ] # si alerte déjà déclenchée
+if [ $flag -eq 1 ] # si alerte déjà déclenchée
 	then
-	if [$4 -lt $seuil] # si la valeur est en dessous du seuil
+	if [ $4 -lt $seuil ] # si la valeur est en dessous du seuil
 	 then
 	 let "temps_redescendu++"
-     if [ $temps_redescendu -eq $battement] # si plusieurs valeurs sont en dessous du seuil, on désactive l'alerte
+        else
+          let "temps_redescendu=0"
+        fi   
+        if [ $temps_redescendu -eq $battement ] # si plusieurs valeurs sont en dessous du seuil, on désactive l'alerte
      	then
      		let "temps_atteint=0"
      		let "temps_redescendu=0"
      		let "flag=0"
      fi
-    fi
 else # l'alerte n'est pas déclenchée
-	then
-     if [$4 -gt $seuil] # valeur au dessus du seuil
+     if [ $4 -gt $seuil ] # valeur au dessus du seuil
      	then
          let "temps_atteint++"
-         if [$temps_atteint -eq $seuil] # si plusieurs valeurs au dessus de seuil, on déclenche l'alerte
+         if [ $temps_atteint -eq $battement ] # si plusieurs valeurs au dessus de seuil, on déclenche l'alerte
             then
              let "flag=1"
          fi
     fi
 fi
+echo $nom_item $seuil $battement $temps_atteint $temps_redescendu $flag
+sed -i "/^$2$3/ d" alerte
+echo $nom_item:$seuil:$battement:$temps_atteint:$temps_redescendu:$flag >>alerte
