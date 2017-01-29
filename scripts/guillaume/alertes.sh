a7b07bbe419a141eadc6cacb23a42f38466670a0
guigui2121 <jauffret@intechinfo.fr>
2017-01-30 00:51:27 +0100

    gjhgfj

--- scripts/guillaume/alertes.sh
+++ scripts/guillaume/alertes.sh
@@ -30,7 +30,7 @@ if [ $flag -eq 1 ] # si alerte déjà déclenchée
      		let "flag=0"
      fi
 else # l'alerte n'est pas déclenchée
-    result = `echo $3 > $seuil | bc`
+    result=`echo $3 > $seuil | bc`
      if [ $result ] # valeur au dessus du seuil
      	then
          let "temps_atteint++"
