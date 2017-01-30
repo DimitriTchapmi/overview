98a1774dbc2e13b1cfcb9f9293d7c0b232000472
guigui2121 <jauffret@intechinfo.fr>
2017-01-30 22:33:00 +0100

    modif chemin

--- sites/index.php
+++ sites/index.php
@@ -11,7 +11,7 @@ if($_SERVER["REQUEST_URI"] == "/"){
         include_once("code/sites/controleur/gestion.php");
 }elseif($_SERVER["REQUEST_URI"] == "/inventaire"){
         include_once("code/sites/controleur/tableau_bord.php");
-}elseif(preg_match("/^\/overview\/machine/",$_SERVER["REQUEST_URI"])){
+}elseif(preg_match("/^\/code/sites/overview\/machine/",$_SERVER["REQUEST_URI"])){
         include_once("code/sites/controleur/machine.php");
 }elseif($_SERVER["REQUEST_URI"] == "/gestion_test"){
         include_once("controleur/gestion_test.php");
