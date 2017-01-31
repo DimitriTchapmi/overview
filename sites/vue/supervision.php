25305a4bfb2d4533a7a4458479ced73a24d5a8a6
tjchris34 <thiburce@intechinfo.fr>
2017-01-31 17:20:05 +0100

    modif vue supervision

--- sites/vue/supervision.php
+++ sites/vue/supervision.php
@@ -3,13 +3,11 @@ include ('header.php');
 ?>
 
 <div class="tabcontainer text-center">
-  <img src="img/eventizi.png">
-    <p><em>Supervision : graphes</em></p>
     <div> <h3> Graphes sur une heure </h3>
     <?php
         echo '<div class="row">';
     	for($i=0;$i<count($graphes["heure"]);$i++){
-    		echo "<div class='col-lg-5' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></div>";
+    		echo "<div class='col-lg-7' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></div>";
     	}
         echo '</div>';
     ?>
@@ -18,7 +16,7 @@ include ('header.php');
     <?php
      echo '<div class="row">';
     	for($i=0;$i<count($graphes["jour"]);$i++){
-    		echo "<div class='col-lg-5' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></div>";
+    		echo "<div class='col-lg-7' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></div>";
     	}
         echo '</div>';
     ?>
@@ -27,7 +25,7 @@ include ('header.php');
     <?php
         echo '<div class="row">';
     	for($i=0;$i<count($graphes["semaine"]);$i++){
-    		echo "<div class='col-lg-5' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></div>";
+    		echo "<div class='col-lg-7' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></div>";
     	}
     	echo '</div>';
     ?>
