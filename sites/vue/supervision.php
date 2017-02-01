568710454e30cbbce191791e28f07023f831671e
tjchris34 <thiburce@intechinfo.fr>
2017-02-01 10:31:38 +0100

    modif vue tab inventaire

--- sites/vue/supervision.php
+++ sites/vue/supervision.php
@@ -5,23 +5,26 @@ include ('header.php');
 <div class="tabcontainer text-center">
     <div> <h3> Graphes sur une heure </h3>
     <?php
+    echo "<div class='col-lg-14' style='margin-top: 10px;'>";
+
     	for($i=0;$i<count($graphes["heure"]);$i++){
-    		echo "<div class='col-lg-14' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></div>";
+    		echo "<img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></div>";
     	}
             ?>
 
     <div> <h3> Graphes sur une journée </h3>
     <?php
+    echo "<div class='col-lg-14' style='margin-top: 10px;'>";
          	for($i=0;$i<count($graphes["jour"]);$i++){
-    		echo "<div class='col-lg-14' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></div>";
+    		echo "<img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></div>";
     	}
         
     ?>
 
     <div> <h3> Graphes sur une semaine </h3>
     <?php
-        
+        echo"<div class='col-lg-14' style='margin-top: 10px;'>";
     	for($i=0;$i<count($graphes["semaine"]);$i++){
-    		echo "<div class='col-lg-14' style='margin-top: 10px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></div>";
+    		echo "<img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></div>";
     	}
     ?>
