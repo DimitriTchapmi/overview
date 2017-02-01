e9347647eb833bd4b2dd1b81942d05fa9e891698
guigui2121 <jauffret@intechinfo.fr>
2017-02-01 17:56:22 +0100

    kjk

--- sites/controleur/gestion.php
+++ sites/controleur/gestion.php
@@ -3,33 +3,27 @@ include("code/sites/modele/inventaire.php");
 include("code/sites/modele/serveur.php");
 
 	if(isset($_SESSION["id_user"])){
+                $infos = getMachineById($_POST["machine"][0]);
+                $entreprise = getEntrepriseById($_SESSION["id_user"]);
 		if(isset($_POST["action"])){
-		$infos = getMachineById($_POST["machine"][0]);
-                $groupes_machine = liste_groupes($_SESSION["id_user"]);
-                $gr_machine = GetGroupeByMachine($infos["id"])["groupe"];
-                $pos = array_search($gr_machine,$groupes_machine);
-                unset($groupes_machine[$pos]);
-                var_dump($groupes_machine);
-                array_values($groupes_machine);
-                var_dump($groupes_machine);
-
-		$tags_gestion = "";
-		$tags_machine = getTagsByMachine($_POST["machine"][0]);
-			for($i=0;$i<count($tags_machine);$i++) {
-				$tags_gestion = $tags_gestion."<button class='btn btn-default tags' value='".$i."'>".$tags_machine[$i]["nom"]."</button> <input type='hidden' name='tags[]' id='tag".$i."' value='".$tags_machine[$i]["nom"]."'>";
-			}		
-        include("code/sites/vue/gestion_test.php");
-        }elseif (isset($_POST["add_groupe"])) {
-        	$nom_epse = getEntrepriseById($_SESSION["id_user"])["nom"];
-        	exec("sudo mkdir projets/".$nom_epse."/inventaire/".$_POST["nom"]);
-        	include("code/sites/vue/connexion.php");
-        }elseif (isset($_POST["add_tags"])) {
-        	foreach ($_POST["machine"] as $machine){
-        	add_tag($_POST["tag"],$machine);
-        	}
-        }
-	}else{
-		include("code/sites/vue/connexion.php");
+                        $groupes_machine = liste_groupes($entreprise);
+                        $gr_machine = GetGroupeByMachine($infos["id"])["groupe"];
+		      $tags_gestion = "";
+		      $tags_machine = getTagsByMachine($_POST["machine"][0]);
+		      for($i=0;$i<count($tags_machine);$i++) {
+			$tags_gestion = $tags_gestion."<button class='btn btn-default tags' value='".$i."'>".$tags_machine[$i]["nom"]."</button> <input type='hidden' name='tags[]' id='tag".$i."' value='".$tags_machine[$i]["nom"]."'>";
+		      }		
+                        include("code/sites/vue/gestion_test.php");
+                }elseif (isset($_POST["add_groupe"])) {
+        	       exec("sudo mkdir projets/".$nom_epse."/inventaire/".$_POST["nom"]);
+        	       include("code/sites/vue/connexion.php");
+                }elseif (isset($_POST["add_tags"])) {
+        	       foreach ($_POST["machine"] as $machine){
+        	       add_tag($_POST["tag"],$machine);
+        	       }
+                }
+        }else{
+                include("code/sites/vue/connexion.php");
 	}
 	
 
