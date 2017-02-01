5d2470fa234cb109baa26ebc71e48e0b1f875361
guigui2121 <jauffret@intechinfo.fr>
2017-02-01 15:28:20 +0100

    jlkj

--- sites/controleur/gestion.php
+++ sites/controleur/gestion.php
@@ -4,10 +4,10 @@ include("code/sites/modele/serveur.php");
 
 	if(isset($_SESSION["id_user"])){
 		if(isset($_POST["action"])){
-		$infos = GetMachineById($_POST["machine"][0]);
+		$infos = GetEntrepriseById($_POST["machine"][0]);
                 $groupes_machine = liste_groupes($infos["nom"]);
                 $gr_machine = GetGroupeByMachine($infos["id"])["groupe"];
-                $pos = array_search($groupes_machine,$gr_machine);
+                $pos = array_search($gr_machine,$groupes_machine,);
                 var_dump($gr_machine,$groupes_machine,$infos["nom"]);
                 unset($groupes_machine[$pos]);
                 array_values($groupes_machine);
