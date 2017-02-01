5e612136748219f9f3c1c9a16f4b9b61ac69f6ab
guigui2121 <jauffret@intechinfo.fr>
2017-02-01 10:38:52 +0100

    modif vue

--- sites/modele/serveur.php
+++ sites/modele/serveur.php
@@ -31,7 +31,10 @@ function ajouter_alerte($entreprise,$machine,$infos){
 	exec("echo ".$item.":".$infos["seuil"].":".$infos["battement"].":0:0:0 >> projets/".$entreprise."/supervision/".$machine."/alertes");
 }
 
-
+function modifier_alerte($entreprise,$machine,$infos){
+	exec("sed -i '/^".$infos["item"]."/ d' /var/www/overview/projets/".$entreprise."/supervision/".$machine."/alertes");
+	exec("echo ".$infos["item"].":".$infos["seuil"].":".$infos["battement"].":0:0:0 >> /var/www/overview/projets/".$entreprise."/supervision/".$machine."/alertes");
+}
 
 function recuperer_inventaire($entreprise, $machine, $groupe){
 	exec("cat projets/".$entreprise."/inventaire/".$groupe."/$machine/".$entreprise."_".$machine,$lignes);
