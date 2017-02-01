9e13034a6e39ec096abe79279868d0b9185051ef
tjchris34 <thiburce@intechinfo.fr>
2017-02-01 14:06:32 +0100

    modif vue supervision

--- sites/vue/supervision.php
+++ sites/vue/supervision.php
@@ -6,11 +6,11 @@ include ('header.php');
 <div class="row"> 
     <h3> Graphes sur une heure </h3>
     <?php
-    echo "<table class='table table-striped'>";
+    echo "<table class='table table-striped' style='width: 55%;'>";
     	for($i=0;$i<count($graphes["heure"]);$i++){
     		echo "<tbody>";
             echo "<tr>";
-            echo "<td style='width: 300px; height: 100px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></td>";
+            echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></td>";
     	    echo "</tr>";
             echo "</tbody>";
         }
@@ -20,11 +20,11 @@ include ('header.php');
     <div class="row">
     <h3> Graphes sur une journée </h3>
     <?php
-    echo "<table class='table table-striped'>";
+    echo "<table class='table table-striped' style='width: 55%;'>";
          	for($i=0;$i<count($graphes["jour"]);$i++){
             echo "<tbody>";
             echo "<tr>";
-            echo "<td style='width: 300px; height: 100px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></td>";
+            echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></td>";
     	    echo "</tr>";
             echo "</tbody>";  
         }
@@ -34,11 +34,11 @@ include ('header.php');
     <div class="row">
     <h3> Graphes sur une semaine </h3>
     <?php
-        echo "<table class='table table-striped'>";
+        echo "<table class='table table-striped' style='width: 55%;'>";
     	for($i=0;$i<count($graphes["semaine"]);$i++){
             echo "<tbody>";
             echo "<tr>";
-            echo "<td style='width: 300px; height: 100px;'><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></td>";
+            echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></td>";
     	    echo "</tr>";
             echo "</tbody>";
         }
