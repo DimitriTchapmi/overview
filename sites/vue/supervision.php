db95d61d758ece0c24a490c85867db6706ef7ada
tjchris34 <thiburce@intechinfo.fr>
2017-02-01 15:06:00 +0100

    modif vue supervision

--- sites/vue/supervision.php
+++ sites/vue/supervision.php
@@ -7,23 +7,23 @@ include ('header.php');
     <?php
     echo "<table class='table table-striped' style='width: 55%;'>";
     	echo "<tbody>";
-        echo "<tr>";
         for($i=0;$i<count($graphes["heure"]);$i++){
+            echo "<tr>";
             echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["heure"][$i]."'></td>";
+            echo "</tr>";    
         }
-        echo "</tr>";
         echo "</tbody>";
         echo "</table>";
             ?> 
     <h3> Graphes sur une journée </h3>
     <?php
     echo "<table class='table table-striped' style='width: 55%;'>";
-         	echo "<tr>";
             echo "<tbody>";
             for($i=0;$i<count($graphes["jour"]);$i++){
+            echo "<tr>";    
             echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["jour"][$i]."'></td>"; 
+            echo "</tr>";
         }
-        echo "</tr>";
         echo "</tbody>"; 
         echo "</table>";
     ?>
@@ -31,11 +31,12 @@ include ('header.php');
     <?php
         echo "<table class='table table-striped' style='width: 55%;'>";
     	echo "<tbody>";
-        echo "<tr>";
         for($i=0;$i<count($graphes["semaine"]);$i++){ 
+            echo "<tr>";
             echo "<td><img src='projets/".$nom_entreprise."/supervision/".$nom_machine."/graphes/".$graphes["semaine"][$i]."'></td>";
+            echo "</tr>";
         }
-        echo "</tr>";
+        
         echo "</tbody>";
         echo "</table>";
     ?>
