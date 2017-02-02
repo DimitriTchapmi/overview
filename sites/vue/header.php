da6d55f55050668009937fd83d8c6ef8a6c29b1a
guigui2121 <jauffret@intechinfo.fr>
2017-02-02 17:26:25 +0100

    modif pages co_in

--- sites/vue/header.php
+++ sites/vue/header.php
@@ -150,7 +150,7 @@ input[type=text]:focus {
   <div class="login-triangle"></div>
   <h2 class="login-header">Connexion</h2>
   <form class="form-horizontal" action="connexion" role="form" method="post">
-    <p><input type="text" name="nom" id="login" placeholder="Pseudo"></p>
+    <p><input name="nom" id="login" placeholder="Entreprise"></p>
     <p><input type="password" name="pass" id="pass" placeholder="Mot de passe"></p>
     <p><input type="submit" name="action" value="Connexion"></p>
   </form>
@@ -162,7 +162,7 @@ input[type=text]:focus {
   <div class="login-triangle"></div>
   <h2 class="login-header">Inscription</h2>
   <form class="form-horizontal" action="inscription" role="form" method="post">
-    <p><input type="text" name="nom" id="login" placeholder="Pseudo"></p>
+    <p><input name="nom" id="login" placeholder="Entreprise"></p>
     <p><input type="password" name="pass1" id="pass" placeholder="Mot de passe"></p>
     <p><input type="password" name="pass2" id="pass" placeholder="Confirmez mot de passe"></p>
     <p><input type="email" name="mail" id="pass" placeholder="Email"></p>
